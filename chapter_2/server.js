bc8e04b758301a764d3eb8487e1478a3e1369554
Eugene Afriyie <groupeight00@gmail.com>
2025-10-17 23:59:40 +0000

    i dependencirs

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,5 +1,15 @@
+
+// The address of this server will be http://localhost:3000
+// url: http://localhost:3000
+// 
 const express = require('express');
 const app = express();
-const PORT = 2002;
+const PORT = 3000;
 
-app.listen(PORT,() => console.log(`SERVER IS RUNNING ON ${PORT}` ));
+
+console.log('Hello World');
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
