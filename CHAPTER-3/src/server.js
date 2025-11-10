995e7e6c01413b964516eee10c6af952c59e25f8
Eugene Afriyie <groupeight00@gmail.com>
2025-11-10 16:04:18 +0000

    i flags

---
+++ CHAPTER-3/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+console.log("Eugwnw")
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port : ${P
